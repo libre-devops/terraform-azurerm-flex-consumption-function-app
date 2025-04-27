b8bf8bd0afc5d25fce11a481272671055f41f341
Craig Thacker <craigthackerx@gmail.com>
2025-04-27 18:54:39 +0100

    Update code

--- examples/module-development/build.tf
+++ examples/module-development/build.tf
@@ -1,19 +1,351 @@
-module "rg" {
-  source = "registry.terraform.io/libre-devops/rg/azurerm"
+locals {
+  rg_name                         = "rg-${var.short}-${var.loc}-${var.env}-01"
+  vnet_name                       = "vnet-${var.short}-${var.loc}-${var.env}-01"
+  function_app_subnet_name        = "FlexFunctionAppSubnet"
+  function_app_integration_subnet = "FlexFunctionAppIntegrationSubnet"
+  key_vault_subnet_name           = "KeyVaultSubnet"
+  storage_subnet_name             = "StorageSubnet"
+  nsg_name                        = "nsg-${var.short}-${var.loc}-${var.env}-01"
+  ssh_public_key_name             = "ssh-${var.short}-${var.loc}-${var.env}-01"
+  admin_username                  = "Local${title(var.short)}${title(var.env)}Admin"
+  user_assigned_identity_name     = "uid-${var.short}-${var.loc}-${var.env}-01"
+  key_vault_name                  = "kv-${var.short}-${var.loc}-${var.env}-01"
+  function_app_name               = "func-${var.short}-${var.loc}-${var.env}-01"
+  log_analytics_name              = "law-${var.short}-${var.loc}-${var.env}-01"
+  storage_account_name            = "sa${var.short}${var.loc}${var.env}01"
 
-  rg_name  = "rg-${var.short}-${var.loc}-${var.env}-${random_string.entropy.result}"
-  location = local.location
-  tags     = local.tags
-
-  #  lock_level = "CanNotDelete" // Do not set this value to skip lock
+  function_app_outbound_ips = flatten([
+    for ips in values(module.flex_linux_function_app.function_apps_possible_outbound_ip_addresses) :
+    split(",", ips)
+  ])
 }
 
-module "dev" {
-  source = "../../"
+module "rg" {
+  source = "libre-devops/rg/azurerm"
+
+  rg_name  = local.rg_name
+  location = local.location
+  tags     = local.tags
+}
+
+module "shared_vars" {
+  source = "libre-devops/shared-vars/azurerm"
+}
+
+locals {
+  lookup_cidr = {
+    for landing_zone, envs in module.shared_vars.cidrs : landing_zone => {
+      for env, cidr in envs : env => cidr
+    }
+  }
+}
+
+module "subnet_calculator" {
+  source = "libre-devops/subnet-calculator/null"
+
+  base_cidr = local.lookup_cidr[var.short][var.env][0]
+  subnets = {
+    (local.function_app_subnet_name) = {
+      mask_size = 26
+      netnum    = 0
+    }
+    (local.function_app_integration_subnet) = {
+      mask_size = 26
+      netnum    = 1
+    }
+    (local.key_vault_subnet_name) = {
+      mask_size = 26
+      netnum    = 2
+    },
+    (local.storage_subnet_name) = {
+      mask_size = 26
+      netnum    = 3
+    }
+  }
+}
+
+module "network" {
+  source = "libre-devops/network/azurerm"
 
   rg_name  = module.rg.rg_name
   location = module.rg.rg_location
   tags     = module.rg.rg_tags
 
-  name = "${var.name}-${random_string.entropy.result}"
+  vnet_name          = local.vnet_name
+  vnet_location      = module.rg.rg_location
+  vnet_address_space = [module.subnet_calculator.base_cidr]
+
+  subnets = {
+    for i, name in module.subnet_calculator.subnet_names :
+    name => {
+      address_prefixes  = toset([module.subnet_calculator.subnet_ranges[i]])
+      service_endpoints = name == local.key_vault_subnet_name ? ["Microsoft.KeyVault"] : name == local.storage_subnet_name ? ["Microsoft.Storage"] : []
+
+      # Only assign delegation to subnet3
+      delegation = name == local.function_app_subnet_name ? [
+        {
+          type = "Microsoft.Web/serverFarms"
+        }
+      ] : []
+    }
+  }
+}
+
+
+module "nsg" {
+  source = "libre-devops/nsg/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  nsg_name              = local.nsg_name
+  associate_with_subnet = true
+  subnet_ids            = { for k, v in module.network.subnets_ids : k => v if k != "AzureBastionSubnet" }
+  custom_nsg_rules = {
+    "AllowVnetInbound" = {
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "VirtualNetwork"
+    }
+    "AllowClientInbound" = {
+      priority                   = 101
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = chomp(data.http.user_ip.response_body)
+      destination_address_prefix = "VirtualNetwork"
+    }
+  }
+}
+
+module "user_assigned_managed_identity" {
+  source = "libre-devops/user-assigned-managed-identity/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  user_assigned_managed_identities = [
+    {
+      name = local.user_assigned_identity_name
+    }
+  ]
+}
+
+module "key_vault" {
+  source = "github.com/libre-devops/terraform-azurerm-keyvault"
+
+  key_vaults = [
+    {
+      rg_name  = module.rg.rg_name
+      location = module.rg.rg_location
+      tags     = module.rg.rg_tags
+
+      name                            = local.key_vault_name
+      enabled_for_deployment          = true
+      enabled_for_disk_encryption     = true
+      enabled_for_template_deployment = true
+      enable_rbac_authorization       = true
+      purge_protection_enabled        = false
+      public_network_access_enabled   = true
+      network_acls = {
+        default_action             = "Deny"
+        bypass                     = "AzureServices"
+        ip_rules                   = [chomp(data.http.user_ip.response_body)]
+        virtual_network_subnet_ids = [module.network.subnets_ids[local.key_vault_subnet_name]]
+      }
+    }
+  ]
+}
+
+module "role_assignments" {
+  source = "github.com/libre-devops/terraform-azurerm-role-assignment"
+
+  role_assignments = [
+    {
+      principal_ids = [data.azurerm_client_config.current.object_id]
+      role_names    = ["Key Vault Administrator"]
+      scope         = module.rg.rg_id
+    },
+    {
+      principal_ids = [module.user_assigned_managed_identity.managed_identity_principal_ids[local.user_assigned_identity_name]]
+      role_names    = ["Key Vault Administrator"]
+      scope         = module.rg.rg_id
+    }
+  ]
+}
+
+module "ssh_keys" {
+  source = "libre-devops/ssh-key/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  ssh_keys = [
+    {
+      name = local.ssh_public_key_name
+    }
+  ]
+}
+
+module "key_vault_secrets" {
+  source = "github.com/libre-devops/terraform-azurerm-key-vault-secrets"
+
+  key_vault_id = module.key_vault.key_vault_ids[0]
+
+  key_vault_secrets = [
+    {
+      secret_name              = "${local.admin_username}-password"
+      generate_random_password = true
+      content_type             = "text/plain"
+      tags                     = module.rg.rg_tags
+    },
+    {
+      secret_name              = "${local.admin_username}-ssh-private-key"
+      generate_random_password = false
+      secret_value             = module.ssh_keys.private_keys_openssh[local.ssh_public_key_name]
+      content_type             = "text/plain"
+      tags                     = module.rg.rg_tags
+    },
+  ]
+}
+
+
+module "sa" {
+  source = "registry.terraform.io/libre-devops/storage-account/azurerm"
+  storage_accounts = [
+    {
+      rg_name  = module.rg.rg_name
+      location = module.rg.rg_location
+      tags     = module.rg.rg_tags
+
+      name          = local.storage_account_name
+      identity_type = "SystemAssigned"
+
+      shared_access_keys_enabled                      = false
+      create_diagnostic_settings                      = true
+      diagnostic_settings_enable_all_logs_and_metrics = false
+      diagnostic_settings = {
+        law_id = module.law.law_id
+        metric = [
+          {
+            category = "Transaction"
+          }
+        ]
+      }
+    },
+  ]
+}
+
+resource "azurerm_storage_container" "flex_function_app_container" {
+  name                  = lower(replace("${local.function_app_name}flexname", "-", ""))
+  storage_account_id    = module.sa.storage_account_ids[local.storage_account_name]
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_account_network_rules" "rules" {
+  default_action     = "Deny"
+  storage_account_id = module.sa.storage_account_ids[local.storage_account_name]
+  ip_rules           = concat([chomp(data.http.user_ip.response_body)], local.function_app_outbound_ips)
+  virtual_network_subnet_ids = [
+    module.network.subnets_ids[local.storage_subnet_name],
+  ]
+}
+
+module "law" {
+  source = "libre-devops/log-analytics-workspace/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  law_name                   = local.log_analytics_name
+  law_sku                    = "PerGB2018"
+  retention_in_days          = "30"
+  daily_quota_gb             = "0.5"
+  internet_ingestion_enabled = false
+  internet_query_enabled     = false
+}
+
+module "flex_linux_function_app" {
+  source = "../../"
+
+  depends_on = [module.law]
+
+  # Application Insights Configuration
+  flex_function_apps = [
+    {
+      rg_name  = module.rg.rg_name
+      location = module.rg.rg_location
+      tags     = module.rg.rg_tags
+
+      name = local.function_app_name
+
+      app_settings = {
+        "FUNCTIONS_WORKER_RUNTIME"               = "python"
+        "AzureWebJobsStorage__accountName"       = module.sa.storage_account_names[local.storage_account_name]
+        "WEBSITE_USE_PLACEHOLDER_DOTNETISOLATED" = "1"
+        "AzureSubscriptionId"                    = data.azurerm_client_config.current.subscription_id          # Replace with actual value
+        "StorageAccountResourceGroup"            = module.rg.rg_name                                           # Replace with actual value
+        "StorageAccountName"                     = module.sa.storage_account_names[local.storage_account_name] # Replace with actual value
+        "KeyVaultUri"                            = module.key_vault.key_vault_uris[0]
+      }
+      public_network_access_enabled = true
+      virtual_network_subnet_id     = module.network.subnets_ids[local.function_app_subnet_name]
+      identity_type                 = "SystemAssigned"
+      storage_container_endpoint    = "${module.sa.primary_blob_endpoints[local.storage_account_name]}${azurerm_storage_container.flex_function_app_container.name}"
+      runtime_name                  = "python"
+      runtime_version               = "3.11"
+
+
+      create_new_app_insights                            = true
+      workspace_id                                       = module.law.law_id
+      app_insights_name                                  = "appi-${local.function_app_name}"
+      app_insights_type                                  = "web"
+      app_insights_daily_cap_in_gb                       = 0.5
+      app_insights_daily_data_cap_notifications_disabled = false
+      app_insights_internet_ingestion_enabled            = true
+      app_insights_internet_query_enabled                = true
+      app_insights_local_authentication_disabled         = true
+      app_insights_sampling_percentage                   = 100
+
+      # Site Configuration
+      site_config = {
+        use_32_bit_worker = false
+        worker_count      = 1
+        cors = {
+          allowed_origins = ["https://portal.azure.com"]
+        }
+        ip_restriction = [
+          {
+            name       = "Allow-Client-IP"
+            priority   = 100     # any unique value 100-65500
+            action     = "Allow" # Allow | Deny
+            ip_address = "${chomp(data.http.user_ip.response_body)}/32"
+          }
+        ]
+      }
+    }
+  ]
+}
+
+module "function_role_assignments" {
+  source = "github.com/libre-devops/terraform-azurerm-role-assignment"
+
+  role_assignments = [
+    {
+      principal_ids = [module.flex_linux_function_app.function_app_identities[local.function_app_name].principal_id]
+      role_names    = ["Key Vault Administrator", "Storage Blob Data Owner", "Storage Blob Data Reader", "Key Vault Secrets Officer", "Storage Account Contributor"]
+      scope         = module.rg.rg_id
+    },
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,258 @@
-variable "location" {
-  description = "The location for this resource to be put in"
-  type        = string
-}
+variable "flex_function_apps" {
+  description = "List of Flex‑Consumption Function Apps (keeps original style)"
+  type = list(object({
+    # ── Core identity ─────────────────────────────────────────────────
+    name        = string
+    rg_name     = string
+    location    = string
 
-variable "name" {
-  type        = string
-  description = "The name of the VNet gateway"
-}
+    # ── Plan creation ─────────────────────────────────────────────────
+    create_new_app_service_plan = optional(bool, true)
+    app_service_plan_name       = optional(string)
+    service_plan_id             = optional(string)
+    os_type                     = optional(string, "Linux")
+    sku_name                    = optional(string, "FC1")
 
-variable "rg_name" {
-  description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
-  type        = string
-}
+    # ── Flex‑specific mandatory fields ───────────────────────────────
+    runtime_name                = string                 # "dotnet-isolated" | "python" | "node" | "java"
+    runtime_version             = string                 # e.g. "8.0", "3.11"
+    storage_container_type      = optional(string, "blobContainer")
+    storage_user_assigned_identity_id = optional(string)
+    storage_container_endpoint  = string                 # "https://<account>.blob.core.windows.net/<container>"
+    storage_authentication_type = optional(string, "SystemAssignedIdentity") # or "StorageAccountConnectionString"
+    storage_access_key          = optional(string)       # only when auth type is connection string
+    maximum_instance_count      = optional(number)       # default from portal (100) if omitted
+    instance_memory_in_mb       = optional(number, 2048) # must be 2048 or 4096
 
-variable "tags" {
-  type        = map(string)
-  description = "A map of the tags to use on the resources that are deployed with this module."
+    app_settings                       = map(string)
+    tags                               = optional(map(string))
+    client_certificate_enabled         = optional(bool)
+    client_certificate_exclusion_paths = optional(string)
+    client_certificate_mode            = optional(string)
+    enabled                            = optional(bool, true)
+    content_share_force_disabled       = optional(bool)
+    identity_type                      = optional(string)
+    public_network_access_enabled      = optional(bool, true)
+    virtual_network_subnet_id          = optional(string)
+    webdeploy_publish_basic_authentication_enabled = optional(bool, false)
+    zip_deploy_file                    = optional(string)
+
+    identity_ids                 = optional(list(string))
+
+    # ── Application Insights options (unchanged) ─────────────────────
+    create_new_app_insights = optional(bool, false)
+    workspace_id            = optional(string)
+    app_insights_name       = optional(string)
+    app_insights_type       = optional(string, "Web")
+    app_insights_daily_cap_in_gb                       = optional(number)
+    app_insights_daily_data_cap_notifications_disabled = optional(bool, false)
+    app_insights_internet_ingestion_enabled            = optional(bool)
+    app_insights_internet_query_enabled                = optional(bool)
+    app_insights_local_authentication_disabled         = optional(bool, true)
+    app_insights_force_customer_storage_for_profile    = optional(bool, false)
+    app_insights_sampling_percentage                   = optional(number, 100)
+
+    sticky_settings = optional(object({
+      app_setting_names       = optional(list(string))
+      connection_string_names = optional(list(string))
+    }))
+
+    connection_string = optional(object({
+      name  = optional(string)
+      type  = optional(string)
+      value = optional(string)
+    }))
+    auth_settings_v2 = optional(object({
+      auth_enabled                            = optional(bool)
+      runtime_version                         = optional(string)
+      config_file_path                        = optional(string)
+      require_authentication                  = optional(bool)
+      unauthenticated_action                  = optional(string)
+      default_provider                        = optional(string)
+      excluded_paths                          = optional(list(string))
+      require_https                           = optional(bool)
+      http_route_api_prefix                   = optional(string)
+      forward_proxy_convention                = optional(string)
+      forward_proxy_custom_host_header_name   = optional(string)
+      forward_proxy_custom_scheme_header_name = optional(string)
+      apple_v2 = optional(object({
+        client_id                  = string
+        client_secret_setting_name = string
+        login_scopes               = list(string)
+      }))
+      active_directory_v2 = optional(object({
+        client_id                            = string
+        tenant_auth_endpoint                 = string
+        client_secret_setting_name           = optional(string)
+        client_secret_certificate_thumbprint = optional(string)
+        jwt_allowed_groups                   = optional(list(string))
+        jwt_allowed_client_applications      = optional(list(string))
+        www_authentication_disabled          = optional(bool)
+        allowed_groups                       = optional(list(string))
+        allowed_identities                   = optional(list(string))
+        allowed_applications                 = optional(list(string))
+        login_parameters                     = optional(map(string))
+        allowed_audiences                    = optional(list(string))
+      }))
+      azure_static_web_app_v2 = optional(object({
+        client_id = string
+      }))
+      custom_oidc_v2 = optional(list(object({
+        name                          = string
+        client_id                     = string
+        openid_configuration_endpoint = string
+        name_claim_type               = optional(string)
+        scopes                        = optional(list(string))
+        client_credential_method      = string
+        client_secret_setting_name    = string
+        authorisation_endpoint        = string
+        token_endpoint                = string
+        issuer_endpoint               = string
+        certification_uri             = string
+      })))
+      facebook_v2 = optional(object({
+        app_id                  = string
+        app_secret_setting_name = string
+        graph_api_version       = optional(string)
+        login_scopes            = optional(list(string))
+      }))
+      github_v2 = optional(object({
+        client_id                  = string
+        client_secret_setting_name = string
+        login_scopes               = optional(list(string))
+      }))
+      google_v2 = optional(object({
+        client_id                  = string
+        client_secret_setting_name = string
+        allowed_audiences          = optional(list(string))
+        login_scopes               = optional(list(string))
+      }))
+      microsoft_v2 = optional(object({
+        client_id                  = string
+        client_secret_setting_name = string
+        allowed_audiences          = optional(list(string))
+        login_scopes               = optional(list(string))
+      }))
+      twitter_v2 = optional(object({
+        consumer_key                 = string
+        consumer_secret_setting_name = string
+      }))
+      login = optional(object({
+        logout_endpoint                   = optional(string)
+        token_store_enabled               = optional(bool)
+        token_refresh_extension_time      = optional(number)
+        token_store_path                  = optional(string)
+        token_store_sas_setting_name      = optional(string)
+        preserve_url_fragments_for_logins = optional(bool)
+        allowed_external_redirect_urls    = optional(list(string))
+        cookie_expiration_convention      = optional(string)
+        cookie_expiration_time            = optional(string)
+        validate_nonce                    = optional(bool)
+        nonce_expiration_time             = optional(string)
+      }))
+    }))
+    auth_settings = optional(object({
+      enabled                        = optional(bool)
+      additional_login_parameters    = optional(map(string))
+      allowed_external_redirect_urls = optional(list(string))
+      default_provider               = optional(string)
+      issuer                         = optional(string)
+      runtime_version                = optional(string)
+      token_refresh_extension_hours  = optional(number)
+      token_store_enabled            = optional(bool)
+      unauthenticated_client_action  = optional(string)
+      active_directory = optional(object({
+        client_id         = optional(string)
+        client_secret     = optional(string)
+        allowed_audiences = optional(list(string))
+      }))
+      facebook = optional(object({
+        app_id       = optional(string)
+        app_secret   = optional(string)
+        oauth_scopes = optional(list(string))
+      }))
+      google = optional(object({
+        client_id     = optional(string)
+        client_secret = optional(string)
+        oauth_scopes  = optional(list(string))
+      }))
+      microsoft = optional(object({
+        client_id     = optional(string)
+        client_secret = optional(string)
+        oauth_scopes  = optional(list(string))
+      }))
+      twitter = optional(object({
+        consumer_key    = optional(string)
+        consumer_secret = optional(string)
+      }))
+      github = optional(object({
+        client_id                  = optional(string)
+        client_secret              = optional(string)
+        client_secret_setting_name = optional(string)
+        oauth_scopes               = optional(list(string))
+      }))
+    }))
+    site_config = optional(object({
+      api_definition_url                            = optional(string)
+      api_management_api_id                         = optional(string)
+      app_command_line                              = optional(string)
+      application_insights_connection_string        = optional(string)
+      application_insights_key                      = optional(string)
+      container_registry_managed_identity_client_id = optional(string)
+      container_registry_use_managed_identity       = optional(bool)
+      elastic_instance_minimum                      = optional(number)
+      health_check_path                             = optional(string)
+      health_check_eviction_time_in_min             = optional(number)
+      http2_enabled                                 = optional(bool)
+      load_balancing_mode                           = optional(string)
+      managed_pipeline_mode                         = optional(string)
+      minimum_tls_version                           = optional(string)
+      remote_debugging_enabled                      = optional(bool)
+      remote_debugging_version                      = optional(string)
+      runtime_scale_monitoring_enabled              = optional(bool)
+      scm_minimum_tls_version                       = optional(string)
+      scm_use_main_ip_restriction                   = optional(bool)
+      use_32_bit_worker                             = optional(bool)
+      websockets_enabled                            = optional(bool)
+      worker_count                                  = optional(number)
+      default_documents                             = optional(list(string))
+      app_service_logs = optional(object({
+        disk_quota_mb         = optional(number)
+        retention_period_days = optional(number)
+      }))
+      cors = optional(object({
+        allowed_origins     = optional(list(string))
+        support_credentials = optional(bool)
+      }))
+      ip_restriction = optional(list(object({
+        ip_address                = optional(string)
+        service_tag               = optional(string)
+        virtual_network_subnet_id = optional(string)
+        name                      = optional(string)
+        priority                  = optional(number)
+        action                    = optional(string)
+        headers = optional(object({
+          x_azure_fdid      = optional(string)
+          x_fd_health_probe = optional(string)
+          x_forwarded_for   = optional(string)
+          x_forwarded_host  = optional(string)
+        }))
+      })))
+      scm_ip_restriction = optional(list(object({
+        ip_address                = optional(string)
+        service_tag               = optional(string)
+        virtual_network_subnet_id = optional(string)
+        name                      = optional(string)
+        priority                  = optional(number)
+        action                    = optional(string)
+        headers = optional(object({
+          x_azure_fdid      = optional(string)
+          x_fd_health_probe = optional(string)
+          x_forwarded_for   = optional(string)
+          x_forwarded_host  = optional(string)
+        }))
+      })))
+    }))
+  }))
+  default = []
 }
